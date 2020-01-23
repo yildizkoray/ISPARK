06e06a29f316fd33197e7754a9761b93e9354f2f
yildizkoray <koray.yildizz@gmail.com>
2020-01-23 23:35:54 +0300

    Infinite Scroll Pagination Added
    UIScrollView_InfiniteScroll

--- ISPARK/ViewController.swift
+++ ISPARK/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import ObjectMapper
 import Alamofire
 import AlamofireObjectMapper
+import UIScrollView_InfiniteScroll
 
 class ViewController: UIViewController {
 
@@ -21,13 +22,45 @@ class ViewController: UIViewController {
     }
   }
 
+  var hasNext: String = ""
+
   override func viewDidLoad() {
     super.viewDidLoad()
     fetchPark()
+
+    tableView.addInfiniteScroll { [unowned self] (tableView) in
+      self.next(next: self.hasNext)
+      tableView.finishInfiniteScroll()
+    }
+
+    tableView.setShouldShowInfiniteScrollHandler { [unowned self] tableView -> Bool in
+      return !self.hasNext.isEmpty
+    }
+  }
+
+  private func next(next: String){
+
+    let url: URL = URL(string: "https://data.ibb.gov.tr\(next)")!
+
+    AF.request(url).responseObject { [weak self] (response: AFDataResponse<ParkResponse>) in
+
+      switch response.result {
+
+      case .success(let data):
+        print("Records: ", data)
+        self?.parks.append(contentsOf: data.records)
+        self?.hasNext = data.next
+        self?.tableView.reloadData()
+
+
+      case .failure(let error):
+        print(error)
+      }
+    }
   }
 
   private func fetchPark() {
-    let url: URL = URL(string: "https://data.ibb.gov.tr/api/3/action/datastore_search?resource_id=c3eb0d72-1ce4-4983-a3a8-6b0b4b19fcb9")!
+    let url: URL = URL(string: "https://data.ibb.gov.tr/api/3/action/datastore_search?resource_id=c3eb0d72-1ce4-4983-a3a8-6b0b4b19fcb9&limit=5")!
 
     AF.request(url).responseObject { [weak self] (response: AFDataResponse<ParkResponse>) in
 
@@ -36,6 +69,7 @@ class ViewController: UIViewController {
       case .success(let data):
         print("Records: ", data)
         self?.parks = data.records
+        self?.hasNext = data.next
 
       case .failure(let error):
         print(error)
@@ -60,14 +94,15 @@ extension ViewController: UITableViewDataSource {
 public struct ParkResponse: ImmutableMappable {
 
   let records: [Park]
+  let next: String
 
   public init(map: Map) throws {
     records = try map.value("result.records")
+    next = try map.value("result._links.next")
   }
 }
 
 public struct Park: ImmutableMappable {
-
 
   let name: String
   let location: String
