aef7688f320563b24dbc2589e733ac656b23c15e
Koray Yıldız <koray.yildizz@gmail.com>
2020-03-24 00:02:49 +0300

    Try something

--- ISPARK/Scenes/Home/UI/Views/ParkItemCell/ParkItemCell.swift
+++ ISPARK/Scenes/Home/UI/Views/ParkItemCell/ParkItemCell.swift
@@ -7,26 +7,58 @@
 //
 
 import UIKit
+import Kingfisher
 
 public final class ParkItemCell: UITableViewCell {
 
   static let identifer = String(describing: self)
 
-  @IBOutlet private weak var icon: UIImageView!
+  private let cache = ImageCache.default
+
   @IBOutlet private weak var titleLabel: UILabel!
   @IBOutlet private weak var subtitleLabel: UILabel!
 
-  override public func awakeFromNib() {
-        super.awakeFromNib()
+  @IBOutlet private weak var icon: UIImageView! {
+    didSet {
+      icon.kf.indicatorType = .activity
     }
+  }
+
+  override public func awakeFromNib() {
+    super.awakeFromNib()
+  }
 
   override public func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-    }
+    super.setSelected(selected, animated: animated)
+  }
 
-  public func configure(with parkItem: ParkItem){
+  public func configure(with parkItem: ParkItem) {
+    
     titleLabel.text = parkItem.name
     subtitleLabel.text = parkItem.location
-    icon.image = UIImage(named: String(Int.random(in: 0..<6)))
+
+    let imageId = String(Int.random(in: 0..<100))
+    let url: URL = URL(string: "https://i.picsum.photos/id/\(imageId)/50/50.jpg")!
+
+    let resource = ImageResource(downloadURL: url)
+
+    if cache.isCached(forKey: resource.cacheKey) {
+
+      cache.retrieveImage(forKey: resource.cacheKey) { [weak self] result in
+
+        switch result {
+
+        case .success(let value):
+          self?.icon.image = value.image
+
+        case .failure(let error):
+          print(error)
+        }
+      }
+    }
+    else {
+      let processor = RoundCornerImageProcessor(cornerRadius: 20)
+      icon.kf.setImage(with: url, options: [.transition(.fade(0.2)), .processor(processor)])
+    }
   }
 }
